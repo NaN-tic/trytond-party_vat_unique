2e5ed5c53911669470702df96fef5b4835125081
resteve <resteve@zikzakmedia.com>
2012-07-18 11:07:40 +0200

    Change copyright notice files

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,5 @@
-#This file is part of Tryton.  The COPYRIGHT file at the top level of
-#this repository contains the full copyright notices and license terms.
+#This file is part party_vat_unique module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains 
+#the full copyright notices and license terms.
 
 from .test_party_vat_unique import suite
