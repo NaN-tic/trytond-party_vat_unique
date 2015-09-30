474d0b48bbf29cf30c5b5e371e5e9e45d689816e
resteve <resteve@zikzakmedia.com>
2015-09-30 14:23:02 +0200

    Change vat_number to identifier party unique
    http://hg.tryton.org/modules/party/rev/aeb2d1398456

--- party.py
+++ party.py
@@ -2,20 +2,25 @@
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
 from trytond.pool import PoolMeta
+from trytond.model import Unique
 
-__all__ = ['Party']
+__all__ = ['PartyIdentifier']
 __metaclass__ = PoolMeta
 
 
-class Party:
-    __name__ = 'party.party'
+class PartyIdentifier:
+    __name__ = 'party.identifier'
 
     @classmethod
     def __setup__(cls):
-        super(Party, cls).__setup__()
-        cls._sql_constraints.extend([
-            ('vat_uniq', 'UNIQUE(vat_number, vat_country)',
-             'There is another party with the same VAT.\n'
-             'The VAT of the party must be unique!')
-        ])
+        super(PartyIdentifier, cls).__setup__()
+        t = cls.__table__()
+        cls._sql_constraints += [
+            ('number_uniq', Unique(t, t.type, t.code),
+                'There is another code with the same number.\n'
+                'The code of the party must be unique!'),
+        ]
 
+    @staticmethod
+    def default_type():
+        return 'eu_vat'
