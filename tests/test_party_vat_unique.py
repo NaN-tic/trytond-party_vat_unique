b3ef688da4ffac14b6854cd1c1d025858bae1624
resteve <resteve@zikzakmedia.com>
2015-12-01 11:12:04 +0100

    Tests

--- tests/test_party_vat_unique.py
+++ tests/test_party_vat_unique.py
@@ -1,21 +1,14 @@
-#!/usr/bin/env python
-# This file is part party_vat_unique module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the party_vat_unique module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class PartyVatUniqueTestCase(unittest.TestCase):
-    'Test PartyVatUnique module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('party_vat_unique')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class PartyVatUniqueTestCase(ModuleTestCase):
+    'Test Party Vat Unique module'
+    module = 'party_vat_unique'
 
 
 def suite():
