a3b3073436713aad3433be2f706825d75a9b1086
Sergi Almacellas Abellana <sergi@koolpi.com>
2016-11-21 12:51:44 +0100

    Add unitest

--- tests/test_party_vat_unique.py
+++ tests/test_party_vat_unique.py
@@ -3,12 +3,38 @@
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import ModuleTestCase, with_transaction
+from trytond.exceptions import UserError
+from trytond.pool import Pool
 
 
 class PartyVatUniqueTestCase(ModuleTestCase):
     'Test Party Vat Unique module'
     module = 'party_vat_unique'
+
+    @with_transaction()
+    def test_identifier_unique(self):
+        'Test Identifier uniqueness'
+        pool = Pool()
+        Party = pool.get('party.party')
+        Identifier = pool.get('party.identifier')
+
+        party = Party(name='test')
+
+        identifier = Identifier(party=party, code='ES47690558N',
+            type='eu_vat')
+        identifier.save()
+
+        duplicated_identifier = Identifier(party=party, code='ES47690558N',
+            type='eu_vat')
+        with self.assertRaises(UserError):
+            duplicated_identifier.save()
+        # Test with diferent party
+        new_party = Party(name='test')
+        new_party.save()
+        duplicated_identifier.party = new_party
+        with self.assertRaises(UserError):
+            duplicated_identifier.save()
 
 
 def suite():
