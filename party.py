3d3dfe694be876327e35f36a916da4550e0268cc
Albert Cervera i Areny <albert@nan-tic.com>
2018-08-24 12:20:25 +0200

    Migrate to python 3

--- party.py
+++ party.py
@@ -7,8 +7,7 @@ from trytond.model import Unique
 __all__ = ['Party', 'PartyIdentifier']
 
 
-class Party:
-    __metaclass__ = PoolMeta
+class Party(metaclass=PoolMeta):
     __name__ = 'party.party'
 
     @classmethod
@@ -19,8 +18,7 @@ class Party:
         return super(Party, cls).copy(parties, default=default)
 
 
-class PartyIdentifier:
-    __metaclass__ = PoolMeta
+class PartyIdentifier(metaclass=PoolMeta):
     __name__ = 'party.identifier'
 
     @classmethod
