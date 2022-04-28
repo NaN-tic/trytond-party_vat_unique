b8403f6af8e865e9dfade6cb14da98dee5e0e329
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-28 12:39:18 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,9 +0,0 @@
-# This file is part party_vat_unique module for Tryton.  The COPYRIGHT file at
-# the top level of this repository contains the full copyright notices and
-# license terms.
-try:
-    from trytond.modules.party_vat_unique.tests.test_party_vat_unique import suite
-except ImportError:
-    from .test_party_vat_unique import suite
-
-__all__ = ['suite']
--- tests/test_module.py
+++ tests/test_module.py
@@ -1,18 +1,14 @@
-# This file is part of the party_vat_unique module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
-import unittest
-import doctest
-import trytond.tests.test_tryton
+
+# This file is part of Tryton.  The COPYRIGHT file at the top level of
+# this repository contains the full copyright notices and license terms.
+
 from trytond.tests.test_tryton import ModuleTestCase, with_transaction
-from trytond.tests.test_tryton import doctest_teardown
-from trytond.tests.test_tryton import doctest_checker
 from trytond.pool import Pool
 from trytond.exceptions import UserError
 
 
 class PartyVatUniqueTestCase(ModuleTestCase):
-    'Test Party Vat Unique module'
+    'Test PartyVatUnique module'
     module = 'party_vat_unique'
 
     @with_transaction()
@@ -56,13 +52,4 @@ class PartyVatUniqueTestCase(ModuleTestCase):
             duplicated_identifier.save()
 
 
-def suite():
-    suite = trytond.tests.test_tryton.suite()
-    suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
-        PartyVatUniqueTestCase))
-    suite.addTests(doctest.DocFileSuite(
-            'scenario_party_replace.rst',
-            tearDown=doctest_teardown, encoding='utf-8',
-            checker=doctest_checker,
-            optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
-    return suite
+del ModuleTestCase
